37720741c741ea4839393f085d8ce4658eedc47b
xx <414122303@qq.com>
2020-10-12 15:32:07 +0800

    project 02  online-edu  END

---
+++ parent/edu-ucService/src/main/java/com/guli/edu_ucService/config/Myconfig.java
@@ -0,0 +1,12 @@
+//package com.guli.edu_ucService.config;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+//@SpringBootConfiguration
+//@EnableTransactionManagement
+//@MapperScan("com.guli.edu_ucService.mapper")
+//public class Myconfig {
+//}
